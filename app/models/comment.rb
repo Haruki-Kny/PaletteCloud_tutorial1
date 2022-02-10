b979897e9ebc464682581a1c4ede0c38070eef9e
Haruki-Kny <harukikonii@gmail.com>
2022-02-10 15:53:22 +0900

    modified 2022/2/10

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   # class_name 試行錯誤中です．
-  # belongs_to :original_post, class_name: 'Post', required: true 
-  belongs_to :post, required: true
+  belongs_to :post, class_name: 'Post', required: true 
+  #belongs_to :post, required: true
   validates :body, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2022_02_05_065956) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
-    t.integer "post_id"
+    t.integer "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
